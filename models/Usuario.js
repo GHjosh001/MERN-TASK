a70e93faa79aeade0bef9afc133998e4170e890d
josh001 <josue.artilesmota@gmail.com>
2020-07-29 19:00:07 -0400

    ajuste para deployment 2

--- models/Usuario.js
+++ models/Usuario.js
@@ -12,8 +12,7 @@ const UsuariosSchema = mongoose.Schema({
     email:{
         type:String,
         required:true,
-        trim:true,
-        unique:true
+        trim:true
     },
 
     password:{
