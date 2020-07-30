b13c4c69e61b4969603cc68ee60c388ce39a202a
josh001 <josue.artilesmota@gmail.com>
2020-07-29 20:57:31 -0400

    ajuste para deployment 4

--- index.js
+++ index.js
@@ -34,5 +34,5 @@ app.use("/api/tareas", require("./routes/tareas"));
 
 //listen server on point
 app.listen(port, '0.0.0.0', () => {
-  console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+  console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
